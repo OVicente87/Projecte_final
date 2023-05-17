20e2daa162f509b1a2871992b707be2fbaec290a
OVicente87 <130688750+OVicente87@users.noreply.github.com>
2023-05-17 11:31:52 +0200

    Update and rename divisió.java to division..java

--- division..java
+++ division..java
@@ -20,3 +20,18 @@ public Complejo division(Complejo divisor) {
           // Crea un nuevo objeto 'Complejo' con la parte real e imaginaria resultante  y lo retorna
         return new Complejo(real, imaginario);
     }
+
+
+// EJEMPLO DE EJECUCIÓN:
+
+// Crear un objeto Complejo para el dividendo con parte real 2 y parte imaginaria 3
+Complejo dividendo = new Complejo(2, 3);
+
+// Crear un objeto Complejo para el divisor con parte real 1 y parte imaginaria 1
+Complejo divisor = new Complejo(1, 1);
+
+// Llamar al método division del objeto dividendo pasando como argumento el objeto divisor
+Complejo resultado = dividendo.division(divisor);
+
+// Imprimir el resultado de la división en la consola
+System.out.println("Resultado de la división: " + resultado.getParteReal() + " + " + resultado.getParteImaginaria() + "i");
